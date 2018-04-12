800f699a584d6dd4ac939665c85afc5a883d4cc8
lb_chen <Administrator@lb_chen>
2018-04-12 17:00:08 +0800

    update

--- src/main/java/com/beini/controller/BaseController.java
+++ src/main/java/com/beini/controller/BaseController.java
@@ -8,18 +8,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.beini.gen_entity.ProductBrand;
+/**
+ * 基本的控制器类
+ * @author lb_chen
+ *
+ */
+
 @RestController
 public class BaseController {
-	
+
 	@GetMapping("/demo")
-    public String demo2(@Valid ProductBrand productBrand, BindingResult result){
-       StringBuffer bf = new StringBuffer();
-		if(result.hasErrors()){
-            for (ObjectError error : result.getAllErrors()) {
-               bf.append(error.getDefaultMessage());
-               bf.append("<br>");
-            }
-        }
+	public String demo2(@Valid ProductBrand productBrand, BindingResult result) {
+		StringBuffer bf = new StringBuffer();
+		if (result.hasErrors()) {
+			for (ObjectError error : result.getAllErrors()) {
+				bf.append(error.getDefaultMessage());
+				bf.append("<br>");
+			}
+		}
 		return bf.toString();
-    }
+	}
 }
